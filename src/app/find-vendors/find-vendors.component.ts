64468a2793b69a6d789474f9fd4fbf55ee764397
Harrison Hutton <harrison.hutton6@gmail.com>
2023-12-08 11:07:24 -0500

    🐛 Unsubscribe to the vendor list OnDestroy

--- src/app/find-vendors/find-vendors.component.ts
+++ src/app/find-vendors/find-vendors.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { DividerModule } from 'primeng/divider';
+import { Subscription } from 'rxjs';
 
 type Vendor = {
   firstname: string,
@@ -18,11 +19,12 @@ type Vendor = {
   templateUrl: './find-vendors.component.html',
   styleUrls: ['./find-vendors.component.scss']
 })
-export class FindVendorsComponent {
+export class FindVendorsComponent implements OnDestroy{
 
   /* Load in all the vendors from the firestore database. They are in
    * the uploadedVendors collection. */
   private vendorsCollectionRef = collection(this.firestore, 'uploadedVendors');
+  private subscription: Subscription | undefined;
 
   vendorList: Vendor[] = [];
 
@@ -35,7 +37,12 @@ export class FindVendorsComponent {
   }
 
   private fetchVendorList(): void {
-    collectionData(this.vendorsCollectionRef, {idField: 'id'})
+    // Unsubscribe from the previous subscription if it exists
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+
+    this.subscription = collectionData(this.vendorsCollectionRef, {idField: 'id'})
     .subscribe(vendors => {
       this.vendorList = vendors as Vendor[];
       this.vendorList.forEach(vendor => {
@@ -50,5 +57,12 @@ export class FindVendorsComponent {
         this.locationsToVendors[key].sort((a, b) => a.locationId.localeCompare(b.locationId));
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the subscription to prevent memory leaks
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
