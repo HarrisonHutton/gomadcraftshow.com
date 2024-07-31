efc31951b6f01bdf30f5ab17e55d19c98e9c1581
Harrison Hutton <harrison.hutton6@gmail.com>
2024-07-31 12:00:31 -0400

    ✨ Add text to Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Contact() {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="px-4 md:px-8">
+            <div className="mx-auto max-w-7xl">
+                <div>
+                    <h1 className="text-7xl font-semibold">Contact</h1>
+                    <p className="mt-4 text-xl">
+                        Message us at{" "}
+                        <Link
+                            href="mailto:gomadcraftshow@gmail.com"
+                            className="text-green-400 transition-all hover:underline"
+                        >
+                            gomadcraftshow@gmail.com
+                        </Link>{" "}
+                        and we'll respond as soon as possible.{" "}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
