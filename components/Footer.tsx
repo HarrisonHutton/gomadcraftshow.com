e7b6a7a14ec81d1adb9e5e59da9c1bc7da29eb06
Harrison Hutton <harrison.hutton6@gmail.com>
2024-07-31 17:03:39 -0400

    💬 Add copyright text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,16 @@ import Link from "next/link";
 export function Footer() {
     return (
         <div className="bg-black text-gray-200">
-            <div className="mx-auto max-w-7xl px-4 py-16 md:px-8">
+            <div className="mx-auto flex max-w-7xl flex-col px-4 py-16 md:px-8">
                 <span>
                     <Link href="/contact">Contact us</Link>
                 </span>
+                <div className="flex w-full">
+                    <span className="my-14 h-[1px] w-full bg-gray-200"></span>
+                </div>
+                <div>
+                    <span>&copy; 2024. GOMAD. All rights reserved.</span>
+                </div>
             </div>
         </div>
     );
