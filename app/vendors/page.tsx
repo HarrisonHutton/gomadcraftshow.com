8a97e87c2143067c04a92c1fb854e885ec532685
Harrison Hutton <harrison.hutton6@gmail.com>
2024-12-14 03:15:31 -0500

    🎨 Use the extracted Vendors component

--- app/vendors/page.tsx
+++ app/vendors/page.tsx
@@ -1,8 +1,4 @@
-import { SelectVendor, vendorsTable } from "@/db/schema";
-import { db } from "@/db/connection";
-import { MapPin, User } from "lucide-react";
-import { FloorMapButton } from "./floor-map-button";
-import "./styles.css";
+import { Vendors } from "./vendors";
 
 // Server component that fetches vendors and renders them
 export default async function Page() {
@@ -11,6 +7,7 @@ export default async function Page() {
             <h1 className="mb-8 text-3xl font-semibold lg:text-7xl">
                 Find the 2024 Vendors!
             </h1>
+            <Vendors />
         </div>
     );
 }
