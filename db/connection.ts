de0097e4724d83ff9c6d6b2d248978391fc13c52
Harrison Hutton <harrison.hutton6@gmail.com>
2024-12-14 03:32:46 -0500

    🔥 Remove unnecessary config

--- db/connection.ts
+++ db/connection.ts
@@ -8,8 +8,5 @@ const connectionString = process.env.DATABASE_URL!;
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, {
     prepare: false,
-    ssl: {
-        rejectUnauthorized: false, // Disable certificate validation
-    },
 });
 export const db = drizzle(client);
